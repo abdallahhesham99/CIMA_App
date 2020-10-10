f7bf41d4af8715f8553002c32f0cebce5b8acd4c
SceneMarketingAgency <scenemarketingagency@gmail.com>
2020-10-10 03:00:18 +0200

    abdallah

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -17,17 +17,15 @@ export class SearchPipe implements PipeTransform {
 
         return movie.title.toLowerCase().indexOf(term.toLowerCase()) !== -1;
 
+      }else if (movie && movie.name) {
+
+        return movie.name.toLowerCase().indexOf(term.toLowerCase()) !== -1;
+
+      }else if (movie && movie.original_name) {
+
+        return movie.original_name.toLowerCase().indexOf(term.toLowerCase()) !== -1;
+
       }
-      // if (movie && movie.name) {
-
-      //   return movie.name.toLowerCase().indexOf(term.toLowerCase()) !== -1;
-
-      // }
-      // if (movie && movie.original_name) {
-
-      //   return movie.original_name.toLowerCase().indexOf(term.toLowerCase()) !== -1;
-
-      // }
 
     })
 
